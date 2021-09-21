d0f2ffa0fe09c84751c3be3e07f31f0208f3c727
jrucld <ysk789465@gmail.com>
2021-09-21 11:17:49 +0900

    BankStatementCSVParser를 도입하여 SRP 적용

--- real_software/src/main/java/BankTransactionAnalyzerSimple.java
+++ real_software/src/main/java/BankTransactionAnalyzerSimple.java
@@ -2,9 +2,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.time.LocalDate;
 import java.time.Month;
-import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BankTransactionAnalyzerSimple {
@@ -14,25 +13,29 @@ public class BankTransactionAnalyzerSimple {
         final Path path = Paths.get(fileName);
         final List<String> lines = Files.readAllLines(path);
 
-        double total = 0d;
-        for (final String line : lines) {
-            final String[] columns = line.split(",");
-            final double amount = Double.parseDouble(columns[1]);
-            total += amount;
-        }
+        final BankStatementCSVParser bankStatementCSVParser = new BankStatementCSVParser();
 
-        System.out.println("모든 거래 내역의 합은 " + total);
+        final List<BankTransaction> bankTransactions = bankStatementCSVParser.parseLinesFromCSV(lines);
 
-        total = 0d;
-        final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-        for (final String line : lines) {
-            final String[] columns = line.split(",");
-            final LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
-            if (date.getMonth() == Month.JANUARY) {
-                final double amount = Double.parseDouble(columns[1]);
-                total += amount;
+        System.out.println("모든 거래 내역의 합은 " + calculateTotalAmount(bankTransactions));
+        System.out.println("1월의 모든 거래 내역은 " + selectInMonth(bankTransactions, Month.JANUARY));
+    }
+
+    private static List<BankTransaction> selectInMonth(final List<BankTransaction> bankTransactions, final Month month) {
+        final List<BankTransaction> bankTransactionInMonth = new ArrayList<>();
+        for (final BankTransaction bankTransaction : bankTransactions) {
+            if (bankTransaction.getDate().getMonth() == month) {
+                bankTransactionInMonth.add(bankTransaction);
             }
         }
-        System.out.println("1월의 모든 거래 내역의 합은 " + total);
+        return bankTransactionInMonth;
+    }
+
+    private static double calculateTotalAmount(final List<BankTransaction> bankTransactions) {
+        double total = 0d;
+        for (final BankTransaction bankTransaction : bankTransactions) {
+            total += bankTransaction.getAmount();
+        }
+        return total;
     }
 }
