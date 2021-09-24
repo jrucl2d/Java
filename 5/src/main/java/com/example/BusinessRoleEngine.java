c0933a651fa077a20937997e7781a3bcaffd1892
jrucl2d <ysk789465@gmail.com>
2021-09-24 17:14:59 +0900

    chapter 5 initial commit

---
+++ 5/src/main/java/com/example/BusinessRoleEngine.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class BusinessRoleEngine {
+    public void addAction(final Action action) {
+        throw new UnsupportedOperationException();
+    }
+
+    public int count() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void run() {
+        throw new UnsupportedOperationException();
+    }
+}
