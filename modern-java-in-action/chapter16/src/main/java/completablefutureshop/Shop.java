d2409662877fea9f5527ae9bcae2e81db569b2bf
jrucld <ysk789465@gmail.com>
2021-10-21 14:23:20 +0900

    completeExceptionally를 사용해 에러를 포함시켜 Future을 종료

--- modern-java-in-action/chapter16/src/main/java/completablefutureshop/Shop.java
+++ modern-java-in-action/chapter16/src/main/java/completablefutureshop/Shop.java
@@ -12,8 +12,12 @@ public class Shop {
     public Future<Double> getPriceAsync(String product) {
         CompletableFuture<Double> futurePrice = new CompletableFuture<>();
         new Thread(() -> {
-            double price = calculatePrice(product); // 다른 스레드에서 비동기적으로 수행
-            futurePrice.complete(price); // 계산 완료되면 Future 에 값 설정
+            try {
+                double price = calculatePrice(product); // 다른 스레드에서 비동기적으로 수행
+                futurePrice.complete(price); // 계산 완료되면 Future 에 값 설정
+            } catch (Exception e) {
+                futurePrice.completeExceptionally(e); // 도중에 문제가 발생하면 발생한 에러를 포함시켜 Future 종료
+            }
         }).start();
         return futurePrice; // 계산 결과의 완료를 기다리지 않고 Future 반환
     }
