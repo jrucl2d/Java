047ea8662f5e50f868f0e4d537058a06d28c30dc
jrucld <ysk789465@gmail.com>
2021-10-22 13:05:43 +0900

    Executor를 사용해 재귀 호출로 인한 스택 오버플로우 해결

--- modern-java-in-action/chapter17/src/main/java/reactive/TempSubscription.java
+++ modern-java-in-action/chapter17/src/main/java/reactive/TempSubscription.java
@@ -1,10 +1,13 @@
 package reactive;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Flow;
 
 public class TempSubscription implements Flow.Subscription {
     private final Flow.Subscriber<? super TempInfo> subscriber;
     private final String town;
+    private static final ExecutorService executor = Executors.newSingleThreadExecutor();
 
     public TempSubscription(Flow.Subscriber<? super TempInfo> subscriber, String town) {
         this.subscriber = subscriber;
@@ -13,14 +16,16 @@ public class TempSubscription implements Flow.Subscription {
 
     @Override
     public void request(long n) {
-        for (long i = 0L; i < n; i++) {
-            try {
-                subscriber.onNext(TempInfo.fetch(town)); // 현재 온도를 subscriber 에게 전송
-            } catch (Exception e) {
-                subscriber.onError(e);
-                break;
+        executor.submit(() -> { // 다른 스레드에서 다음 요소를 구독자에게 보낸다.
+            for (long i = 0L; i < n; i++) {
+                try {
+                    subscriber.onNext(TempInfo.fetch(town));
+                } catch (Exception e) {
+                    subscriber.onError(e);
+                    break;
+                }
             }
-        }
+        });
     }
 
     @Override
