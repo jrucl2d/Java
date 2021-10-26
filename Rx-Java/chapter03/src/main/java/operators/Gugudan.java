b503ca7fe67e3516d2a4616abfbde3871a101f9d
jrucl2d <ysk789465@gmail.com>
2021-10-26 14:23:41 +0900

    flatmap을 다른 방식으로 사용해 구구단 구현

--- Rx-Java/chapter03/src/main/java/operators/Gugudan.java
+++ Rx-Java/chapter03/src/main/java/operators/Gugudan.java
@@ -6,7 +6,8 @@ import io.reactivex.functions.Function;
 import java.util.Scanner;
 
 public class Gugudan {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Scanner in = new Scanner(System.in);
         System.out.println("Gugudan Input : ");
         int dan = Integer.parseInt(in.nextLine());
@@ -14,10 +15,22 @@ public class Gugudan {
         Function<Integer, Observable<String>> func = num ->
             Observable.range(1, 9)
                 .map(row -> num + " * " + row + " = " + row * num);
-
-
         Observable.just(dan)
             .flatMap(func)
             .subscribe(System.out::println);
+        System.out.println("--------------------");
+
+        // 람다를 사용한 버전
+        Observable.just(dan)
+            .flatMap(num -> Observable.range(1, 9)
+                .map(row -> num + " * " + row + " = " + row * num))
+            .subscribe(System.out::println);
+        System.out.println("--------------------");
+
+        Observable.just(dan)
+            .flatMap(gugu -> Observable.range(1, 9),
+                (gugu, i) -> gugu + " * " + i + " = " + gugu * i)
+            .subscribe(System.out::println);
+        in.close();
     }
 }
