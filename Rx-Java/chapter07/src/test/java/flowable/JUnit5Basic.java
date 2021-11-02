cf7e1a2edf32d33257830fe909e0bac9de280936
jrucl2d <ysk789465@gmail.com>
2021-11-02 18:13:33 +0900

    RxJava 메소드 이용한 테스트

--- Rx-Java/chapter07/src/test/java/flowable/JUnit5Basic.java
+++ Rx-Java/chapter07/src/test/java/flowable/JUnit5Basic.java
@@ -35,4 +35,38 @@ class JUnit5Basic {
 
         assertThat(Arrays.asList(expected)).isEqualTo(actual);
     }
+
+    @DisplayName("#1: using TestObserver for Shape")
+    @Test
+    void testGetShapeUsingTestObservable() {
+        String[] data = {"1", "2-R", "3-T"};
+        Observable<String> source = Observable.fromArray(data);
+
+        String[] expected = {"1", "2-R", "3-T"};
+        source.test().assertResult(expected);
+    }
+
+    @DisplayName("assertFailure() example")
+    @Test
+    void assertFailureExample() {
+        String[] data = {"100", "200", "%300"};
+        Observable<Integer> source = Observable.fromArray(data)
+            .map(Integer::parseInt);
+
+        source.test().assertFailure(NumberFormatException.class, 100, 200); // 앞 두 개까지는 정상
+        source.test().assertFailureAndMessage(NumberFormatException.class, "For input string: \"%300\"", 100, 200);
+    }
+
+    @DisplayName("assertComplete() example")
+    @Test
+    void assertComplete() {
+        Observable<String> source = Observable.create(
+            emitter -> {
+                emitter.onNext("Hello RxJava");
+                emitter.onComplete();
+            }
+        );
+        source.test().assertComplete(); // onComplete 알람 보냈는지 확인
+    }
+
 }
