75ea3f67f79adf2e366ffb86bbfc221fa274664c
jrucl2d <ysk789465@gmail.com>
2021-11-02 18:05:59 +0900

    순수한 JUnit 기반 테스트 코드 작성

---
+++ Rx-Java/chapter07/src/test/java/flowable/JUnit5Basic.java
@@ -0,0 +1,38 @@
+package flowable;
+
+import io.reactivex.Observable;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import utils.Log;
+import utils.Shape;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class JUnit5Basic {
+
+    @DisplayName("JUnit 5 First example")
+    @Test
+    void testFirst() {
+        int expected = 3;
+        int actual = 1 + 2;
+        assertThat(expected).isEqualTo(actual);
+    }
+
+    @DisplayName("test getShape() Observable")
+    @Test
+    void testGetShapeObservable() {
+        String[] data = {"1", "2-R", "3-T"};
+        Observable<String> source = Observable.fromArray(data);
+
+        String[] expected = {"1", "2-R", "3-T"};
+        List<String> actual = new ArrayList<>();
+        source.doOnNext(Log::i)
+            .subscribe(actual::add);
+
+        assertThat(Arrays.asList(expected)).isEqualTo(actual);
+    }
+}
