928f3749f1bae78995306090e4e6d0f54f7f19e6
jrucl2d <ysk789465@gmail.com>
2021-11-05 14:34:44 +0900

    Flux에서 에러 처리, Exceptions.propagate 사용

--- Study/reactor/src/test/java/com/example/reactor/Part07Errors.java
+++ Study/reactor/src/test/java/com/example/reactor/Part07Errors.java
@@ -1,7 +1,11 @@
 package com.example.reactor;
 
 import org.junit.jupiter.api.Test;
+import reactor.core.Exceptions;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.Locale;
 
 public class Part07Errors {
 
@@ -20,4 +24,33 @@ public class Part07Errors {
             .doOnNext(System.out::println) // 결과 위와 같은 2가 리턴됨
             .subscribe();
     }
+
+    @Test
+    void name2() {
+        // Flux도 Mono와 같음.
+        Flux<Object> flux = Flux.error(new RuntimeException());
+        flux.log().onErrorResume(e -> Flux.just(2, 3))
+            .doOnNext(System.out::println)
+            .subscribe();
+    }
+
+    @Test
+    void name3() {
+        // Checked Exception은 try catch로 잡아서 RuntimeException으로 감싸서 리턴해야 한다.
+        Mono.just("hello")
+            .log()
+            .map(s -> {
+                try {
+                    return Integer.parseInt(s);
+                } catch (Exception e) {
+                    throw Exceptions.propagate(e); // runtime exception으로 바꿔줌
+                    // StepVerifier나 Subscriber가 까서 볼 수 있음. 그 때 CheckedException으로 보임
+                }
+            })
+            .onErrorReturn(1243)
+            .doOnNext(System.out::println)
+            .subscribe();
+    }
+
+    private static class MyException extends Exception {}
 }
