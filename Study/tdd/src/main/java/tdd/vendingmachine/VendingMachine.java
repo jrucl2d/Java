b77c00d33b7755c23deff51d9ebbe0524bcda0c6
jrucl2d <ysk789465@gmail.com>
2021-11-11 17:44:30 +0900

    자판기에 동전 넣기 TDD 중, coin 객체 추출 필요

--- Study/tdd/src/main/java/tdd/vendingmachine/VendingMachine.java
+++ Study/tdd/src/main/java/tdd/vendingmachine/VendingMachine.java
@@ -6,16 +6,31 @@ import java.util.List;
 public class VendingMachine
 {
     private final List<String> drinks;
+    private int coin;
 
     public VendingMachine() {
         this.drinks = Collections.emptyList();
+        this.coin = 0;
     }
     public VendingMachine(List<String> drinks) {
         this.drinks = drinks;
+        this.coin = 0;
     }
 
     public List<String> getAllDrinks()
     {
         return this.drinks;
+    }
+
+    public void insertCoin(int coin)
+    {
+        if (!(coin == 50 || coin == 100 || coin == 500))
+            throw new IllegalArgumentException();
+        this.coin += coin;
+    }
+
+    public int getInsertedCoin()
+    {
+        return coin;
     }
 }
