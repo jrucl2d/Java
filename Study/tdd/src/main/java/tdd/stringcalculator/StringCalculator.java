521670d551b9b0a8b7f1ad9bd18c91a98a169267
jrucl2d <ysk789465@gmail.com>
2021-11-11 14:13:26 +0900

    리팩토링 진행

--- Study/tdd/src/main/java/tdd/stringcalculator/StringCalculator.java
+++ Study/tdd/src/main/java/tdd/stringcalculator/StringCalculator.java
@@ -2,11 +2,29 @@ package tdd.stringcalculator;
 
 public class StringCalculator {
     public static int splitAndSum(String string) {
-        if (string == null || string.isEmpty()) return 0;
-        String[] split = string.split("[,;]");
+        if (isBlank(string)) return 0;
+        return sum(toInts(string.split("[,;]")));
+    }
+
+    private static boolean isBlank(String string) {
+        if (string == null || string.isEmpty())
+            return true;
+        return false;
+    }
+
+    private static int[] toInts(String[] values) {
+        int[] ret = new int[values.length];
+        int i = 0;
+        for (String value : values) {
+            ret[i++] = Integer.parseInt(value);
+        }
+        return ret;
+    }
+
+    private static int sum(int[] numbers) {
         int ret = 0;
-        for (String s : split) {
-            ret += Integer.parseInt(s);
+        for (int number : numbers) {
+            ret += number;
         }
         return ret;
     }
