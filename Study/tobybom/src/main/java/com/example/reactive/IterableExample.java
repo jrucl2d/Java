fc1e193e1dbe7f71dbc3263143097780e62c95ab
jrucld <ysk789465@gmail.com>
2021-11-21 19:29:31 +0900

    Observable과 쌍대성을 갖는 Iterable

---
+++ Study/tobybom/src/main/java/com/example/reactive/IterableExample.java
@@ -0,0 +1,31 @@
+package com.example.reactive;
+
+import java.util.Iterator;
+
+public class IterableExample {
+    public static void main(String[] args) {
+        Iterable<Integer> iter = () -> new Iterator<>() {
+            int i = 0;
+            final static int MAX = 10;
+
+            @Override
+            public boolean hasNext() {
+                return i < MAX;
+            }
+
+            @Override
+            public Integer next() {
+                return ++i;
+            }
+        };
+
+        for (Integer integer : iter) {
+            System.out.println(integer);
+        }
+
+        // 이전에는
+        for (Iterator<Integer> it = iter.iterator(); it.hasNext();) {
+            System.out.println(it.next());
+        }
+    }
+}
