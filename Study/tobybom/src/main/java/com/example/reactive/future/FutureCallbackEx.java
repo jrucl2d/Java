d15cdf716239a5d8fdefea1d37a0c18701fd5d16
jrucld <ysk789465@gmail.com>
2021-11-24 21:50:52 +0900

    Callback 스타일로 클래스 생성

--- Study/tobybom/src/main/java/com/example/reactive/future/FutureCallbackEx.java
+++ Study/tobybom/src/main/java/com/example/reactive/future/FutureCallbackEx.java
@@ -2,37 +2,56 @@ package com.example.reactive.future;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.FutureTask;
+import java.util.Objects;
+import java.util.concurrent.*;
 
 @Slf4j
 public class FutureCallbackEx {
+    interface SuccessCallback {
+        void onSuccess(String result);
+    }
+
+    interface ExceptionCallback {
+        void onError(Throwable t);
+    }
+
+    public static class CallbackFutureTask extends FutureTask<String> {
+        private SuccessCallback sc;
+        private ExceptionCallback ec;
+
+        public CallbackFutureTask(Callable<String> callable, SuccessCallback sc, ExceptionCallback ec) {
+            super(callable);
+            this.sc = Objects.requireNonNull(sc); // 없으면 NullPointerException
+            this.ec = Objects.requireNonNull(ec);
+        }
+
+        @Override
+        protected void done() {
+            try {
+                sc.onSuccess(get());
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            } catch (ExecutionException e) {
+                ec.onError(e.getCause());
+            }
+        }
+    }
+
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         ExecutorService es = Executors.newCachedThreadPool();
 
-        FutureTask<String> f = new FutureTask<>(() -> {
-            Thread.sleep(2000);
-            log.info("Async");
-            return "Hello";
-        }) {
-            @Override
-            protected void done() {
-                try {
-                    System.out.println(get());
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } catch (ExecutionException e) {
-                    e.printStackTrace();
-                }
-            }
-        };
+        CallbackFutureTask callbackFutureTask = new CallbackFutureTask(
+                () -> {
+                    Thread.sleep(2000);
+                    if (true) throw new RuntimeException("Async Error!!");
+                    log.info("Async");
+                    return "Hello";
+                },
+                log::info,
+                t -> log.error("Error", t)
+        );
 
-        es.execute(f);
-        System.out.println(f.isDone());
-        log.info("Something else"); // 병렬적으로 Async와 같이 돌아감
-        System.out.println(f.isDone());
+        es.execute(callbackFutureTask);
 
         es.shutdown();
     }
