d2f7ec76ba5878429549a48462869737ba3eee26
jrucld <ysk789465@gmail.com>
2021-11-26 15:33:58 +0900

    DeferredResult를 사용. worker 쓰레드가 살아있지 않고 서블릿 자원 최소화하면서 이벤트성에서 비동기 처리

--- Study/tobybom/src/main/java/com/example/reactive/ReactiveApplication.java
+++ Study/tobybom/src/main/java/com/example/reactive/ReactiveApplication.java
@@ -6,24 +6,39 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
 
-import java.util.concurrent.Callable;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedDeque;
 
 @Slf4j
 @EnableAsync // Async 사용하도록
 @SpringBootApplication
 public class ReactiveApplication {
-
     @RestController
     public static class MyController {
+        Queue<DeferredResult> results = new ConcurrentLinkedDeque<>();
+
         @GetMapping("/")
-        public Callable<String> callable() {
-            log.info("callable");
-            return () -> {
-                log.info("async");
-                Thread.sleep(2000);
-                return "hello";
-            };
+        public DeferredResult<String> dr() {
+            log.info("dr");
+            DeferredResult<String> dr = new DeferredResult<>(600000L);
+            results.add(dr);
+            return dr;
+        }
+
+        @GetMapping("/dr/count")
+        public String drCount() {
+            return "" + results.size();
+        }
+
+        @GetMapping("/dr/event")
+        public String drevent(String message) {
+            for (DeferredResult result : results) {
+                result.setResult("Hello" + message);
+                results.remove(result);
+            }
+            return "OK";
         }
     }
 
