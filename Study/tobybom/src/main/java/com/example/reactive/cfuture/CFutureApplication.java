f03550e3857d5f539551e843589ccc410a0ffb24
jrucl2d <ysk789465@gmail.com>
2021-12-03 10:31:05 +0900

    람다에서 메소드 참조로 변경

--- Study/tobybom/src/main/java/com/example/reactive/cfuture/CFutureApplication.java
+++ Study/tobybom/src/main/java/com/example/reactive/cfuture/CFutureApplication.java
@@ -8,7 +8,7 @@ public class CFutureApplication {
     // ListenableFuture를 CompletableFuture로 변경해주는 코드
     <T> CompletableFuture<T> toCF(ListenableFuture<T> lf) {
         CompletableFuture<T> cf = new CompletableFuture<>();
-        lf.addCallback(s -> {cf.complete(s);}, e -> cf.completeExceptionally(e));
+        lf.addCallback(cf::complete, cf::completeExceptionally);
         return cf;
     }
     public static void main(String[] args) {
